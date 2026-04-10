0abb2072e2ddc61c6aa879c38bfe3c13a52c7a64
Lukas Wirth <lukas@zed.dev>
2026-04-10 10:25:29 +0100

    Remove smol as a dependency from a bunch of crates

--- crates/agent_ui/src/threads_archive_view.rs
+++ crates/agent_ui/src/threads_archive_view.rs
@@ -1244,7 +1244,7 @@ impl PickerDelegate for ProjectPickerDelegate {
             })
             .collect();
 
-        let mut sibling_matches = smol::block_on(fuzzy::match_strings(
+        let mut sibling_matches = gpui::block_on(fuzzy::match_strings(
             &sibling_candidates,
             query,
             smart_case,
@@ -1278,7 +1278,7 @@ impl PickerDelegate for ProjectPickerDelegate {
             })
             .collect();
 
-        let mut recent_matches = smol::block_on(fuzzy::match_strings(
+        let mut recent_matches = gpui::block_on(fuzzy::match_strings(
             &recent_candidates,
             query,
             smart_case,
